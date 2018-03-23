cd8f809f5e6774a763c7df59ff4576b6f550b871
Jens Schulze <jens.schulze@commercetools.de>
2018-03-23 14:30:05 +0100

    add strict typing to CtpRequestBuilder

--- src/CtpRequestBuilder.php
+++ src/CtpRequestBuilder.php
@@ -1,19 +1,22 @@
 <?php
+declare(strict_types = 1);
+
 namespace Commercetools;
 
 use Commercetools\Base\Mapper;
 use Commercetools\Client\ApiRequest;
 use Commercetools\Client\Resource;
 use Commercetools\Request\RequestBuilder;
+use Commercetools\Request\Resource0;
 
 class CtpRequestBuilder extends Resource
 {
-    public function __construct($projectKey, Mapper $mapper = null)
+    public function __construct(string $projectKey, Mapper $mapper = null)
     {
         parent::__construct('', ['projectKey' => $projectKey], $mapper);
     }
 
-    public function with()
+    public function with(): Resource0
     {
         $builder = new RequestBuilder($this->getArgs(), $this->getMapper());
         return $builder->withProjectKeyValue();
