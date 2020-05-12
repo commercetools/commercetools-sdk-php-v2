103494aa56a234c683ed39c14ff9770b6f6db643
Barbara Palumbo <barbara.palumbo@commercetools.de>
2020-05-12 14:47:50 +0200

    feat(Category): add Fixture class and first tests some refactoring

--- test/integration/Api/Category/CategoryQueryTest.php
+++ test/integration/Api/Category/CategoryQueryTest.php
@@ -1,69 +1,62 @@
 <?php
 
-
 namespace Commercetools\IntegrationTest\Api\Category;
 
+use Commercetools\Api\Models\Category\Category;
 use Commercetools\Api\Client\ClientCredentialsConfig;
 use Commercetools\Api\Client\Config;
+use Commercetools\Api\Models\Category\CategoryDraft;
 use Commercetools\Api\Models\Category\CategoryPagedQueryResponse;
-use Commercetools\Client\ApiRequestBuilder;
-use Commercetools\Client\ClientCredentials;
-use Commercetools\Client\ClientFactory;
-use Commercetools\Client\MiddlewareFactory;
-use Commercetools\Client\OAuthHandlerFactory;
-use GuzzleHttp\HandlerStack;
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
-use PHPUnit\Framework\TestCase;
+use Commercetools\IntegrationTest\ApiTestCase;
 
-class CategoryQueryTest extends TestCase
+class CategoryQueryTest extends ApiTestCase
 {
-    /**
-     * @var \GuzzleHttp\Client
-     */
-    private $client;
-
-    /**
-     * @var string
-     */
-    private $projectKey;
-
-    /**
-     * @var ApiRequestBuilder
-     */
-    private $builder;
-
-    public function setUp(): void
-    {
-        $clientId = $_SERVER['CTP_CLIENT_ID'] ?? '';
-        $clientSecret = $_SERVER['CTP_CLIENT_SECRET'] ?? '';
-        $this->projectKey = $_SERVER['CTP_PROJECT_KEY'] ?? '';
-
-        $authHandler = HandlerStack::create();
-        $authHandler->push(
-            MiddlewareFactory::createLoggerMiddleware(new Logger('auth', [new StreamHandler('./logs/requests.log')]))
-        );
-
-        $authConfig = new ClientCredentialsConfig(new ClientCredentials($clientId, $clientSecret), [
-            'handler' => $authHandler,
-        ]);
-        $logger = new Logger('client', [new StreamHandler('./logs/requests.log')]);
-
-        $client = ClientFactory::of()->createGuzzleClientForHandler(
-            new Config(['maxRetries' => 3]),
-            OAuthHandlerFactory::ofAuthConfig($authConfig),
-            $logger
-        );
-        $this->client = $client;
-        $this->builder = new ApiRequestBuilder($this->projectKey, $this->client);
-    }
-
     public function testQuery()
     {
-        $request = $this->builder->with()->categories()->get();
+        $builder = $this->getApiBuilder();
 
-        $categoryQueryResponse = $request->execute();
+        CategoryFixture::withCategory(
+            $builder,
+            function (Category $category) use ($builder) {
+                $request = $builder->with()->categories()->get();
+                $categoryQueryResponse = $request->execute();
 
-        $this->assertInstanceOf(CategoryPagedQueryResponse::class, $categoryQueryResponse);
+                $this->assertInstanceOf(CategoryPagedQueryResponse::class, $categoryQueryResponse);
+                $this->assertCount(1, $categoryQueryResponse->getResults());
+            }
+        );
     }
+
+    public function testGetById()
+    {
+        $builder = $this->getApiBuilder();
+
+        CategoryFixture::withCategory(
+            $builder,
+            function (Category $category) use ($builder) {
+                $request = $builder->with()->categories()->withId($category->getId())->get();
+                $categoryQueryResponse = $request->execute();
+
+                $this->assertInstanceOf(Category::class, $categoryQueryResponse);
+                $this->assertSame($category->getId(), $categoryQueryResponse->getId());
+            }
+        );
+    }
+
+    public function testGetByKey()
+    {
+        $builder = $this->getApiBuilder();
+
+        CategoryFixture::withCategory(
+            $builder,
+            function (Category $category) use ($builder) {
+                $request = $builder->with()->categories()->withKey($category->getKey())->get();
+                $categoryQueryResponse = $request->execute();
+
+                $this->assertInstanceOf(Category::class, $categoryQueryResponse);
+                $this->assertSame($category->getId(), $categoryQueryResponse->getId());
+            }
+        );
+    }
+
 }
---
+++ test/integration/ApiTestCase.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Commercetools\IntegrationTest;
+
+use Commercetools\Api\Client\ClientCredentialsConfig;
+use Commercetools\Api\Client\Config;
+use Commercetools\Client\ApiRequestBuilder;
+use Commercetools\Client\ClientCredentials;
+use Commercetools\Client\ClientFactory;
+use Commercetools\Client\MiddlewareFactory;
+use Commercetools\Client\OAuthHandlerFactory;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\HandlerStack;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
+
+class ApiTestCase extends TestCase
+{
+    /**
+     * @var ClientInterface
+     */
+    private static $client;
+    private static $projectKey;
+    public function getApiClient()
+    {
+        if (is_null(self::$client)) {
+            $clientId = $_SERVER['CTP_CLIENT_ID'] ?? '';
+            $clientSecret = $_SERVER['CTP_CLIENT_SECRET'] ?? '';
+            self::$projectKey = $_SERVER['CTP_PROJECT_KEY'] ?? '';
+            $authHandler = HandlerStack::create();
+            $authHandler->push(
+                MiddlewareFactory::createLoggerMiddleware(new Logger('auth', [new StreamHandler('./logs/requests.log')]))
+            );
+            $authConfig = new ClientCredentialsConfig(new ClientCredentials($clientId, $clientSecret), [
+                'handler' => $authHandler,
+            ]);
+            $logger = new Logger('client', [new StreamHandler('./logs/requests.log')]);
+            $client = ClientFactory::of()->createGuzzleClientForHandler(
+                new Config(['maxRetries' => 3]),
+                OAuthHandlerFactory::ofAuthConfig($authConfig),
+                $logger
+            );
+            self::$client = $client;
+        }
+        return self::$client;
+    }
+    public function getApiBuilder()
+    {
+        $client = $this->getApiClient();
+        return new ApiRequestBuilder(self::$projectKey, $client);
+    }
+}
