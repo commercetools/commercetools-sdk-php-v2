09a30bb4ad56cc84216cd30b0f167d06ae1699af
Auto Mation <automation@commercetools.com>
2020-11-24 12:41:43 +0000

    TASK: Updating import SDK

--- lib/commercetools-import/src/Models/Productvariants/ProductVariantPatch.php
+++ lib/commercetools-import/src/Models/Productvariants/ProductVariantPatch.php
@@ -31,7 +31,7 @@ interface ProductVariantPatch extends JsonObject
      * <p>Maps to <code>ProductVariant.attributes</code>.</p>
      * <p>Each attribute referenced must be defined
      * in an already existing product type in the commercetools project, or the import
-     * operation state is set to <code>Unresolved</code>.</p>
+     * operation state is set to <code>ValidationFailed</code>.</p>
      *
      * @return null|Attributes
      */
