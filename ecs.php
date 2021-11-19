753a33ca0756ac41dd9172b7e5ca9d6e1bc17dce
Jens Schulze <jens.schulze@commercetools.com>
2021-11-19 09:01:49 +0100

    update ecs for parallel execution

--- ecs.php
+++ ecs.php
@@ -6,13 +6,15 @@ ini_set("memory_limit", "-1");
 use Commercetools\Tools\SummaryOnlyOutputFormatter;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
-    $parameters->set('cache_directory', __DIR__ . '/.ecs_cache');
+    $parameters->set(Option::CACHE_DIRECTORY, __DIR__ . '/.ecs_cache');
 
-    $parameters->set('paths', [__DIR__ . '/src', __DIR__ . '/test', __DIR__ . '/lib/commercetools-api/src', __DIR__ . '/lib/commercetools-api-tests/test', __DIR__ . '/lib/commercetools-base/src', __DIR__ . '/lib/commercetools-base/test', __DIR__ . '/lib/commercetools-import/src', __DIR__ . '/lib/commercetools-import-tests/test', __DIR__ . '/lib/commercetools-ml/src', __DIR__ . '/lib/commercetools-ml-tests/test']);
+    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/test', __DIR__ . '/lib/commercetools-api/src', __DIR__ . '/lib/commercetools-api-tests/test', __DIR__ . '/lib/commercetools-base/src', __DIR__ . '/lib/commercetools-base/test', __DIR__ . '/lib/commercetools-import/src', __DIR__ . '/lib/commercetools-import-tests/test', __DIR__ . '/lib/commercetools-ml/src', __DIR__ . '/lib/commercetools-ml-tests/test']);
+    $parameters->set(Option::PARALLEL, true);
 
     $containerConfigurator->import(SetList::PSR_12);
 
