7d01f04188d92d2f9dbe8dec20c30d6fee7f98eb
Auto Mation <automation@commercetools.com>
2022-02-02 13:21:08 +0000

    TASK: Updating SDK

--- lib/commercetools-api/src/Models/Zone/ZoneUpdateModel.php
+++ lib/commercetools-api/src/Models/Zone/ZoneUpdateModel.php
@@ -42,7 +42,7 @@ final class ZoneUpdateModel extends JsonObjectModel implements ZoneUpdate
     }
 
     /**
-     * <p>Expected version of the Zone on which the changes should be applied. If the expected version does not match the actual version, a 409 Conflict will be returned.</p>
+     * <p>Expected version of the Zone on which the changes should be applied. If the expected version does not match the actual version, a <a href="/../api/errors#409-conflict">409 Conflict</a> will be returned.</p>
      *
      * @return null|int
      */
