313bbfb598d74fecb05da4524d237d6600b8a6f1
Auto Mation <automation@commercetools.com>
2022-06-02 20:34:38 +0000

    TASK: Updating SDK

---
+++ lib/commercetools-base/src/Client/CacheValidator.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file has been auto generated
+ * Do not change it.
+ */
+
+
+namespace Commercetools\Client;
+
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\SimpleCache\CacheInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
+final class CacheValidator
+{
+    /**
+     * @psalm-param CacheItemPoolInterface|CacheInterface|null $cache
+     * @psalm-return CacheItemPoolInterface|CacheInterface
+     */
+    public static function validateCache($cache = null)
+    {
+        if ($cache instanceof CacheItemPoolInterface || $cache instanceof CacheInterface) {
+            return $cache;
+        }
+
+        if (class_exists('Symfony\Component\Cache\Simple\FilesystemCache')) {
+            /** @psalm-suppress all */
+            /** @var CacheItemPoolInterface $cache */
+            $cache = new FilesystemCache('', 0, getcwd() . "/cache");
+        } else {
+            /** @psalm-suppress all */
+            /** @var CacheItemPoolInterface $cache */
+            $cache = new FilesystemAdapter('', 0, getcwd() . "/cache");
+        }
+
+        return $cache;
+    }
+}
--- lib/commercetools-base/src/Client/OAuthHandlerFactory.php
+++ lib/commercetools-base/src/Client/OAuthHandlerFactory.php
@@ -21,42 +21,24 @@ class OAuthHandlerFactory
 {
     /**
      * @psalm-param CacheItemPoolInterface|CacheInterface|null $cache
-     * @psalm-return CacheItemPoolInterface|CacheInterface
-     */
-    private static function validateCache($cache = null)
-    {
-        if ($cache instanceof CacheItemPoolInterface || $cache instanceof CacheInterface) {
-            return $cache;
-        }
-
-        $filesystemAdapter = new Local(getcwd());
-        $filesystem = new Filesystem($filesystemAdapter);
-        $cache = new FilesystemCachePool($filesystem);
-        
-        return $cache;
-    }
-
-    /**
-     * @psalm-param CacheItemPoolInterface|CacheInterface|null $cache
      */
     public static function ofAuthConfig(AuthConfig $authConfig, $cache = null): OAuth2Handler
     {
-        $cache = self::validateCache($cache);
+        $cache = CacheValidator::validateCache($cache);
         switch (true) {
-           case $authConfig instanceof ClientCredentialsConfig:
-               $provider = new CachedTokenProvider(
-                   new ClientCredentialTokenProvider(
-                       new Client($authConfig->getOptions()),
-                       $authConfig->getAuthUri(),
-                       $authConfig->getCredentials()
-                   ),
-                   $cache,
-                   $authConfig->getCredentials()->getCacheKey()
-               );
-               break;
-           default:
-               throw new InvalidArgumentException('Unknown authorization configuration');
-
+            case $authConfig instanceof ClientCredentialsConfig:
+                $provider = new CachedTokenProvider(
+                    new ClientCredentialTokenProvider(
+                        new Client($authConfig->getOptions()),
+                        $authConfig->getAuthUri(),
+                        $authConfig->getCredentials()
+                    ),
+                    $cache,
+                    $authConfig->getCredentials()->getCacheKey()
+                );
+                break;
+            default:
+                throw new InvalidArgumentException('Unknown authorization configuration');
         }
         return self::ofProvider($provider);
     }
