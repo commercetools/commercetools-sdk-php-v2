7f0fdf9149eafe86c6219cdb7f6bf60f00ba8942
Auto Mation <automation@commercetools.com>
2023-05-17 14:04:07 +0000

    TASK: Updating SDK

--- lib/commercetools-api/src/Models/Product/ProductRemovePriceAction.php
+++ lib/commercetools-api/src/Models/Product/ProductRemovePriceAction.php
@@ -8,12 +8,16 @@ declare(strict_types=1);
 
 namespace Commercetools\Api\Models\Product;
 
+use Commercetools\Api\Models\Common\PriceDraft;
 use Commercetools\Base\DateTimeImmutableCollection;
 use Commercetools\Base\JsonObject;
 
 interface ProductRemovePriceAction extends ProductUpdateAction
 {
     public const FIELD_PRICE_ID = 'priceId';
+    public const FIELD_SKU = 'sku';
+    public const FIELD_VARIANT_ID = 'variantId';
+    public const FIELD_PRICE = 'price';
     public const FIELD_STAGED = 'staged';
 
     /**
@@ -23,6 +27,35 @@ interface ProductRemovePriceAction extends ProductUpdateAction
      * @return null|string
      */
     public function getPriceId();
+
+    /**
+     * <p>The <code>sku</code> of the ProductVariant the provided Price should be removed from.
+     * Either 'variantId' or 'sku' is required&quot; when <code>priceId</code> is not provided.
+     * This field is now deprecated, use 'priceId' instead.</p>
+     *
+
+     * @return null|string
+     */
+    public function getSku();
+
+    /**
+     * <p>The <code>id</code> of the ProductVariant the provided Price should be removed from.
+     * Either 'variantId' or 'sku' is required&quot; when <code>priceId</code> is not provided.
+     * This field is now deprecated, use 'priceId' instead.</p>
+     *
+
+     * @return null|int
+     */
+    public function getVariantId();
+
+    /**
+     * <p>The Price identical to the one to be removed from the ProductVariant.
+     * This field is now deprecated, use 'priceId' instead.</p>
+     *
+
+     * @return null|PriceDraft
+     */
+    public function getPrice();
 
     /**
      * <p>If <code>true</code>, only the staged Embedded Price is removed. If <code>false</code>, both the current and staged Embedded Price are removed.</p>
@@ -36,6 +69,21 @@ interface ProductRemovePriceAction extends ProductUpdateAction
      * @param ?string $priceId
      */
     public function setPriceId(?string $priceId): void;
+
+    /**
+     * @param ?string $sku
+     */
+    public function setSku(?string $sku): void;
+
+    /**
+     * @param ?int $variantId
+     */
+    public function setVariantId(?int $variantId): void;
+
+    /**
+     * @param ?PriceDraft $price
+     */
+    public function setPrice(?PriceDraft $price): void;
 
     /**
      * @param ?bool $staged
