55f7b33773ba45859cbd51968f02afedf389204b
Auto Mation <automation@commercetools.com>
2023-05-23 11:16:17 +0000

    TASK: Updating SDK

--- lib/commercetools-api/src/Models/Project/ExternalOAuthBuilder.php
+++ lib/commercetools-api/src/Models/Project/ExternalOAuthBuilder.php
@@ -33,7 +33,7 @@ final class ExternalOAuthBuilder implements Builder
     private $authorizationHeader;
 
     /**
-     * <p>URL with authorization header.</p>
+     * <p>URL with authorization header. If the Project is hosted in the China (AWS, Ningxia) Region, verify that the URL is not blocked due to firewall restrictions.</p>
      *
 
      * @return null|string
