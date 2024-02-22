61373c3e1f0a0bd5e6ccb339bdad1609e463b47d
Jens Schulze <jens.schulze@commercetools.com>
2024-02-22 09:59:39 +0100

    cleanup config files from ML sources

--- ecs.php
+++ ecs.php
@@ -8,7 +8,7 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->cacheDirectory(__DIR__ . '/.ecs_cache');
-    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/test', __DIR__ . '/lib/commercetools-api/src', __DIR__ . '/lib/commercetools-api-tests/test', __DIR__ . '/lib/commercetools-base/src', __DIR__ . '/lib/commercetools-base/test', __DIR__ . '/lib/commercetools-import/src', __DIR__ . '/lib/commercetools-import-tests/test', __DIR__ . '/lib/commercetools-ml/src', __DIR__ . '/lib/commercetools-ml-tests/test']);
+    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/test', __DIR__ . '/lib/commercetools-api/src', __DIR__ . '/lib/commercetools-api-tests/test', __DIR__ . '/lib/commercetools-base/src', __DIR__ . '/lib/commercetools-base/test', __DIR__ . '/lib/commercetools-import/src', __DIR__ . '/lib/commercetools-import-tests/test']);
     $ecsConfig->parallel();
     $ecsConfig->sets([SetList::PSR_12]);
 };
