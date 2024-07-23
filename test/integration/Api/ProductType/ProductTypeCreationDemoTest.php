f42026e2010011e58cd4c87f60ff2601bab7ccf8
Barbara Palumbo <barbarapalumbo@Barbaras-MacBook-Pro.local>
2024-07-23 07:47:02 +0200

    adding tests for documentation

---
+++ test/integration/Api/ProductType/ProductTypeCreationDemoTest.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace Commercetools\IntegrationTest\Api\ProductType;
+
+use Commercetools\Api\Models\Common\LocalizedStringBuilder;
+use Commercetools\Api\Models\ProductType\AttributeDateTimeTypeBuilder;
+use Commercetools\Api\Models\ProductType\AttributeDefinitionBuilder;
+use Commercetools\Api\Models\ProductType\AttributeDefinitionCollection;
+use Commercetools\Api\Models\ProductType\AttributeDefinitionDraftBuilder;
+use Commercetools\Api\Models\ProductType\AttributeDefinitionDraftCollection;
+use Commercetools\Api\Models\ProductType\AttributeEnumTypeBuilder;
+use Commercetools\Api\Models\ProductType\AttributeLocalizedEnumType;
+use Commercetools\Api\Models\ProductType\AttributeLocalizedEnumTypeBuilder;
+use Commercetools\Api\Models\ProductType\AttributeLocalizedEnumValueBuilder;
+use Commercetools\Api\Models\ProductType\AttributeLocalizedEnumValueCollection;
+use Commercetools\Api\Models\ProductType\AttributeMoneyTypeBuilder;
+use Commercetools\Api\Models\ProductType\AttributePlainEnumValueBuilder;
+use Commercetools\Api\Models\ProductType\AttributePlainEnumValueCollection;
+use Commercetools\Api\Models\ProductType\AttributeReferenceType;
+use Commercetools\Api\Models\ProductType\AttributeReferenceTypeBuilder;
+use Commercetools\Api\Models\ProductType\AttributeSetTypeBuilder;
+use Commercetools\Api\Models\ProductType\AttributeTextTypeBuilder;
+use Commercetools\Api\Models\ProductType\ProductType;
+use Commercetools\Api\Models\ProductType\ProductTypeBuilder;
+use Commercetools\Api\Models\ProductType\ProductTypeDraftBuilder;
+use Commercetools\Exception\NotFoundException;
+use Commercetools\IntegrationTest\ApiTestCase;
+use function Sodium\randombytes_random16;
+
+class ProductTypeCreationDemoTest extends ApiTestCase
+{
+    const ISBN_ATTR_NAME = "AttributeTutorialIsbn";
+    const BOOK_PRODUCT_TYPE_NAME = "book-product-attribute-tutorial";
+    const COLOR_ATTR_NAME = "AttributeTutorialColor";
+    const SIZE_ATTR_NAME = "AttributeTutorialSize";
+    const LAUNDRY_SYMBOLS_ATTR_NAME = "AttributeTutorialLaundrySymbols";
+    const MATCHING_PRODUCTS_ATTR_NAME = "AttributeTutorialMatchingProducts";
+    const RRP_ATTR_NAME = "AttributeTutorialRrp";
+    const AVAILABLE_SINCE_ATTR_NAME = "AttributeTutorialAvailableSince";
+    const PRODUCT_TYPE_NAME = "t-shirt-product-attribute-tutorial";
+    public function createBookProductTypeDraft()
+    {
+        $isbn = AttributeDefinitionBuilder::of()
+            ->withType(AttributeTextTypeBuilder::of()->build())
+            ->withName(self::ISBN_ATTR_NAME)
+            ->withLabel(LocalizedStringBuilder::of(["en" => "ISBN"])->build())
+            ->withIsRequired(false)
+            ->build();
+
+        return $bookProductTypeDraft = ProductTypeDraftBuilder::of()
+            ->withKey(uniqid())
+            ->withName(self::BOOK_PRODUCT_TYPE_NAME)
+            ->withDescription("books")
+            ->withAttributes(AttributeDefinitionDraftCollection::fromArray([$isbn]))
+            ->build();
+    }
+
+    public function createTShirtProductTypeDraft()
+    {
+        $green = AttributeLocalizedEnumValueBuilder::of()
+                    ->withKey("green")
+                    ->withLabel(LocalizedStringBuilder::fromArray(["en" => "green", "de" => "grün"])->build())
+                    ->build();
+        $red = AttributeLocalizedEnumValueBuilder::of()
+                    ->withKey("red")
+                    ->withLabel(LocalizedStringBuilder::fromArray(["en" => "red", "de" => "rot"])->build())
+                    ->build();
+        $color = AttributeDefinitionDraftBuilder::of()
+                    ->withName(self::COLOR_ATTR_NAME)
+                    ->withLabel(LocalizedStringBuilder::fromArray(["en" =>"color"])->build())
+                    ->withType(AttributeLocalizedEnumTypeBuilder::of()
+                                ->withValues(AttributeLocalizedEnumValueCollection::fromArray([$green, $red]))
+                                ->build())
+                    ->withIsRequired(true)
+                    ->build();
+        $small = AttributePlainEnumValueBuilder::of()
+                    ->withKey("S")
+                    ->withLabel("S")
+                    ->build();
+        $medium = AttributePlainEnumValueBuilder::of()
+                    ->withKey("M")
+                    ->withLabel("M")
+                    ->build();
+        $sizeX = AttributePlainEnumValueBuilder::of()
+                    ->withKey("X")
+                    ->withLabel("X")
+                    ->build();
+        $size = AttributeDefinitionDraftBuilder::of()
+                    ->withName(self::SIZE_ATTR_NAME)
+                    ->withLabel(LocalizedStringBuilder::fromArray(["en" => "Size"])->build())
+                    ->withType(AttributeEnumTypeBuilder::of()
+                                    ->withValues(AttributePlainEnumValueCollection::fromArray([$small, $medium, $sizeX]))
+                                    ->build())
+                    ->withIsRequired(true)
+                    ->build();
+        $cold = AttributeLocalizedEnumValueBuilder::of()
+                    ->withKey("cold")
+                    ->withLabel(LocalizedStringBuilder::fromArray(["en" => "Wash at or below 30°C ", "de" => "30°C"])->build())
+                    ->build();
+        $hot = AttributeLocalizedEnumValueBuilder::of()
+                    ->withKey("hot")
+                    ->withLabel(LocalizedStringBuilder::fromArray(["en" => "Wash at or below 60°C", "de" => "60°C"])->build())
+                    ->build();
+        $tumbleDrying = AttributeLocalizedEnumValueBuilder::of()
+                            ->withKey("tumbleDrying")
+                            ->withLabel(LocalizedStringBuilder::fromArray(["en" => "Tumble Drying", "de" => "Trommeltrocknen"])->build())
+                            ->build();
+        $noTumbleDrying = AttributeLocalizedEnumValueBuilder::of()
+                            ->withKey("noTumbleDrying")
+                            ->withLabel(LocalizedStringBuilder::fromArray(["en" => "no tumble drying", "de" => "Nicht im Trommeltrockner trocknen"])->build())
+                            ->build();
+        $laundryLabelType = AttributeSetTypeBuilder::of()
+                                ->withElementType(AttributeLocalizedEnumTypeBuilder::of()
+                                                    ->withValues(AttributeLocalizedEnumValueCollection::fromArray([$cold, $hot, $tumbleDrying, $noTumbleDrying]))
+                                                    ->build())
+                                ->build();
+        $laundrySymbols = AttributeDefinitionDraftBuilder::of()
+                            ->withType($laundryLabelType)
+                            ->withName(self::LAUNDRY_SYMBOLS_ATTR_NAME)
+                            ->withLabel(LocalizedStringBuilder::fromArray(["en" => "washing labels"])->build())
+                            ->withIsRequired(false)
+                            ->build();
+
+        $matchingProducts = AttributeDefinitionDraftBuilder::of()
+                                ->withName(self::MATCHING_PRODUCTS_ATTR_NAME)
+                                ->withLabel(LocalizedStringBuilder::fromArray(["en" => "matching products"])->build())
+                                ->withType(AttributeSetTypeBuilder::of()
+                                            ->withElementType(AttributeReferenceTypeBuilder::of()
+                                                                ->withReferenceTypeId("product")
+                                                                ->build())
+                                            ->build())
+                                ->withIsRequired(false)
+                                ->build();
+        $rrp = AttributeDefinitionDraftBuilder::of()
+                    ->withName(self::RRP_ATTR_NAME)
+                    ->withLabel(LocalizedStringBuilder::fromArray(["en" => "recommended retail price"])->build())
+                    ->withType(AttributeMoneyTypeBuilder::of()->build())
+                    ->withIsRequired(false)
+                    ->build();
+        $availableSince = AttributeDefinitionDraftBuilder::of()
+                            ->withName(self::AVAILABLE_SINCE_ATTR_NAME)
+                            ->withLabel(LocalizedStringBuilder::fromArray(["en" => "available since"])->build())
+                            ->withType(AttributeDateTimeTypeBuilder::of()->build())
+                            ->withIsRequired(false)
+                            ->build();
+        $attributes = AttributeDefinitionDraftCollection::fromArray([$color, $size, $laundrySymbols, $matchingProducts, $rrp, $availableSince]);
+
+        return $productTypeDraft = ProductTypeDraftBuilder::of()
+                                ->withKey(uniqid())
+                                ->withName(self::PRODUCT_TYPE_NAME)
+                                ->withDescription("a 'T' shaped cloth")
+                                ->withAttributes($attributes)
+                                ->build();
+    }
+
+    public function testCreateBookProductType()
+    {
+        $builder = $this->getApiBuilder();
+
+        $bookProductTypeDraft = $this->createBookProductTypeDraft();
+
+        $bookProductType = $builder
+            ->with()
+            ->productTypes()
+            ->post($bookProductTypeDraft)
+            ->execute();
+
+        $request = $builder
+            ->with()
+            ->productTypes()
+            ->withKey($bookProductType->getKey())
+            ->get();
+
+        $bookProductTypeQueryResponse = $request->execute();
+        $this->assertSame($bookProductType->getName(), $bookProductTypeQueryResponse->getName());
+        $this->assertSame($bookProductType->getKey(), $bookProductTypeQueryResponse->getKey());
+        $this->assertSame(1, $bookProductTypeQueryResponse->getVersion());
+    }
+
+    public function testCreateTShirtProductType()
+    {
+        $builder = $this->getApiBuilder();
+
+        $productTypeDraft = $this->createTShirtProductTypeDraft();
+
+        $productType = $builder
+            ->with()
+            ->productTypes()
+            ->post($productTypeDraft)
+            ->execute();
+
+        $request = $builder
+            ->with()
+            ->productTypes()
+            ->withId($productType->getId())
+            ->get();
+        $productTypeQueryResponse = $request->execute();
+        $this->assertSame($productType->getName(), $productTypeQueryResponse->getName());
+        $this->assertSame($productType->getId(), $productTypeQueryResponse->getId());
+        $this->assertSame(1, $productTypeQueryResponse->getVersion());
+    }
+
+
+}
